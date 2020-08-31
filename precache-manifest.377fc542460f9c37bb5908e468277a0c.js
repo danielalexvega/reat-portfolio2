189447fc0222b76d744095a17c5a22035fa2940a
Daniel Vega <danielalexvega@gmail.com>
2020-08-31 09:34:34 -0500

    Updates

---
+++ precache-manifest.377fc542460f9c37bb5908e468277a0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92c20cc33c63ec641c85013626e9287",
+    "url": "/react-portfolio2/index.html"
+  },
+  {
+    "revision": "f0c837748c9e25558301",
+    "url": "/react-portfolio2/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "143d61f014c2b04bce3b",
+    "url": "/react-portfolio2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f0c837748c9e25558301",
+    "url": "/react-portfolio2/static/js/2.eb605714.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio2/static/js/2.eb605714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143d61f014c2b04bce3b",
+    "url": "/react-portfolio2/static/js/main.eb205918.chunk.js"
+  },
+  {
+    "revision": "c08891286cb85014615d",
+    "url": "/react-portfolio2/static/js/runtime-main.8755c4f3.js"
+  }
+]);
